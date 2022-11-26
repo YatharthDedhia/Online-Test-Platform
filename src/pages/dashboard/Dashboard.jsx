cce42bc04b633196ef0262aedc55475ab28d90b8
YatharthDedhia <dedhiayatharth2004@gmail.com>
2022-11-27 01:08:03 +0530

    Added content in calendar

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,4 +1,3 @@
-// import React from 'react';
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import './css/dashboard.css';
@@ -13,19 +12,19 @@ const mockTests = [
 	{
 		name: 'Periodic Test - DBMS',
 		link: 'asd-qwvs-dfs',
-		time: '20/01/2022 17:30',
+		time: '20/11/2022 17:30',
 		status: "unattempted"
 	},
 	{
 		name: 'Periodic Test - OSLT',
 		link: 'pbl-dfse-phd',
-		time: '21/01/2022 17:30',
+		time: '21/11/2022 17:30',
 		status: "unattempted"
 	},
 	{
 		name: 'Periodic Test - SPCC',
 		link: 'fhh-dfgg-aee',
-		time: '22/01/2022 17:30',
+		time: '22/11/2022 17:30',
 		status: 'attempted'
 	}
 ];
@@ -42,38 +41,38 @@ const tabList = [
 	}
 ]
 
-const CommonInput = ({ placeholderText = 'Input', value, onChange }) => {
-	return (
-		<input
-			type="text"
-			placeholder={placeholderText}
-			value={value}
-			onChange={onChange}
-		/>
-	);
-};
+// const CommonInput = ({ placeholderText = 'Input', value, onChange }) => {
+// 	return (
+// 		<input
+// 			type="text"
+// 			placeholder={placeholderText}
+// 			value={value}
+// 			onChange={onChange}
+// 		/>
+// 	);
+// };
 
-const CopyLink = ({ link = 'http://github.com' }) => {
-	return (
-		<div className="copy-link">
-			<div className="copy-link-input">
-				<CommonInput value={link} />
-			</div>
-			<button
-				className="copy-btn"
-				onClick={(e) => {
-					navigator.clipboard.writeText(link);
-					e.target.innerHTML = 'Copied!';
-					setTimeout(function () {
-						e.target.innerHTML = 'Copy';
-					}, 2000);
-				}}
-			>
-				Copy
-			</button>
-		</div>
-	);
-};
+// const CopyLink = ({ link = 'http://github.com' }) => {
+// 	return (
+// 		<div className="copy-link">
+// 			<div className="copy-link-input">
+// 				<CommonInput value={link} />
+// 			</div>
+// 			<button
+// 				className="copy-btn"
+// 				onClick={(e) => {
+// 					navigator.clipboard.writeText(link);
+// 					e.target.innerHTML = 'Copied!';
+// 					setTimeout(function () {
+// 						e.target.innerHTML = 'Copy';
+// 					}, 2000);
+// 				}}
+// 			>
+// 				Copy
+// 			</button>
+// 		</div>
+// 	);
+// };
 
 const NavLinks = () => (
 	<React.Fragment>
@@ -152,7 +151,7 @@ const Table = () => {
 }
 
 const Dashboard = () => {
-	const [value, onChange] = useState(new Date());
+	const [date, setDate] = useState(new Date());
 	return (
 		<React.Fragment>
 			<Navbar />
@@ -171,7 +170,19 @@ const Dashboard = () => {
 					))}
 				</div>
 				<div className="calendar">
-					<Calendar onChange={onChange} value={value} />
+					<Calendar onChange={setDate} value={date} tileContent={
+						({ activeStartDate, date, view }) => {
+							return mockTests.map((test) => (
+								(view === 'month' && date.getDate() === parseInt(test.time.slice(0, 2)) && date.getMonth() === (parseInt(test.time.slice(3, 5)) - 1) && date.getYear() === (parseInt(test.time.slice(6, 10)) - 1900))
+									? <p className='DateContent'
+									// onMouseEnter={
+									// 	// do whatever you want
+									// 	console.log()
+									// }
+									>{date.getDate()} Test Day {(test.time.slice(11, 16))}</p>
+									: null))
+						}
+					} />
 				</div>
 			</div >
 		</React.Fragment>

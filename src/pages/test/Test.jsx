6e16ed7f158b41a160dd5e085d3eef58c582224f
YatharthDedhia <dedhiayatharth2004@gmail.com>
2022-12-04 23:38:06 +0530

    Added quiz

--- src/pages/test/Test.jsx
+++ src/pages/test/Test.jsx
@@ -1,5 +1,7 @@
-// import React from "react";
+import Quiz from 'react-quiz-component';
+// import { quiz } from 'quiz';
 import ReactDOM from "react-dom";
+import './css/test.css'
 import Webcam from 'react-webcam';
 import React, { useState } from 'react';
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
@@ -19,6 +21,104 @@ const renderTime = ({ remainingTime }) => {
     </div>
   );
 };
+
+export const quizzz = {
+  "nrOfQuestions": "4",
+  "questions": [
+    {
+      "question": "How can you access the state of a component from inside of a member function?",
+      "questionType": "text",
+      "questionPic": "https://dummyimage.com/600x400/000/fff&text=X", // if you need to display Picture in Question
+      "answerSelectionType": "single",
+      "answers": [
+        "this.getState()",
+        "this.prototype.stateValue",
+        "this.state",
+        "this.values"
+      ],
+      "correctAnswer": "3",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "point": "20"
+    },
+    {
+      "question": "ReactJS is developed by _____?",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "Google Engineers",
+        "Facebook Engineers"
+      ],
+      "correctAnswer": "2",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "point": "20"
+    },
+    {
+      "question": "ReactJS is an MVC based framework?",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "True",
+        "False"
+      ],
+      "correctAnswer": "2",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "point": "10"
+    },
+    {
+      "question": "Which of the following concepts is/are key to ReactJS?",
+      "questionType": "text",
+      "answerSelectionType": "single",
+      "answers": [
+        "Component-oriented design",
+        "Event delegation model",
+        "Both of the above",
+      ],
+      "correctAnswer": "3",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "point": "30"
+    },
+    {
+      "question": "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+      "questionType": "photo",
+      "answerSelectionType": "single",
+      "answers": [
+        "https://dummyimage.com/600x400/000/fff&text=A",
+        "https://dummyimage.com/600x400/000/fff&text=B",
+        "https://dummyimage.com/600x400/000/fff&text=C",
+        "https://dummyimage.com/600x400/000/fff&text=D"
+      ],
+      "correctAnswer": "1",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "point": "20"
+    },
+    {
+      "question": "What are the advantages of React JS?",
+      "questionType": "text",
+      "answerSelectionType": "multiple",
+      "answers": [
+        "React can be used on client and as well as server side too",
+        "Using React increases readability and makes maintainability easier. Component, Data patterns improves readability and thus makes it easier for manitaining larger apps",
+        "React components have lifecycle events that fall into State/Property Updates",
+        "React can be used with any other framework (Backbone.js, Angular.js) as it is only a view layer"
+      ],
+      "correctAnswer": [1, 2, 4],
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "point": "20"
+    },
+  ]
+}
 
 const children = ({ remainingTime }) => {
   const hours = Math.floor(remainingTime / 3600)
@@ -64,9 +164,92 @@ const WebLiveCapture = () => {
   );
 };
 
+// const Quizz = () => {
+//   const questions = [
+//     {
+//       questionText: 'What is the capital of France?',
+//       answerOptions: [
+//         { answerText: 'New York', isCorrect: false },
+//         { answerText: 'London', isCorrect: false },
+//         { answerText: 'Paris', isCorrect: true },
+//         { answerText: 'Dublin', isCorrect: false },
+//       ],
+//     },
+//     {
+//       questionText: 'Who is CEO of Tesla?',
+//       answerOptions: [
+//         { answerText: 'Jeff Bezos', isCorrect: false },
+//         { answerText: 'Elon Musk', isCorrect: true },
+//         { answerText: 'Bill Gates', isCorrect: false },
+//         { answerText: 'Tony Stark', isCorrect: false },
+//       ],
+//     },
+//     {
+//       questionText: 'The iPhone was created by which company?',
+//       answerOptions: [
+//         { answerText: 'Apple', isCorrect: true },
+//         { answerText: 'Intel', isCorrect: false },
+//         { answerText: 'Amazon', isCorrect: false },
+//         { answerText: 'Microsoft', isCorrect: false },
+//       ],
+//     },
+//     {
+//       questionText: 'How many Harry Potter books are there?',
+//       answerOptions: [
+//         { answerText: '1', isCorrect: false },
+//         { answerText: '4', isCorrect: false },
+//         { answerText: '6', isCorrect: false },
+//         { answerText: '7', isCorrect: true },
+//       ],
+//     },
+//   ];
+
+//   const [currentQuestion, setCurrentQuestion] = useState(0);
+//   const [showScore, setShowScore] = useState(false);
+//   const [score, setScore] = useState(0);
+
+//   const handleAnswerOptionClick = (isCorrect) => {
+//     if (isCorrect) {
+//       setScore(score + 1);
+//     }
+
+//     const nextQuestion = currentQuestion + 1;
+//     if (nextQuestion < questions.length) {
+//       setCurrentQuestion(nextQuestion);
+//     } else {
+//       setShowScore(true);
+//     }
+//   };
+//   return (
+//     <div className='quizz-app'>
+//       {showScore ? (
+//         <div className='score-section'>
+//           You scored {score} out of {questions.length}
+//         </div>
+//       ) : (
+//         <>
+//           <div className='question-section'>
+//             <div className='question-count'>
+//               <span>Question {currentQuestion + 1}</span>/{questions.length}
+//             </div>
+//             <div className='question-text'>{questions[currentQuestion].questionText}</div>
+//           </div>
+//           <div className='answer-section'>
+//             {questions[currentQuestion].answerOptions.map((answerOption) => (
+//               <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+//             ))}
+//           </div>
+//         </>
+//       )}
+//     </div>
+//   );
+// }
+
 const Test = () => {
   return (
     <div className="timer-wrapper">
+      <Quiz quiz={quizzz} shuffle={true}/>
+      {/* <Quizz /> */}
       <CountdownCircleTimer
         isPlaying
         duration={60}

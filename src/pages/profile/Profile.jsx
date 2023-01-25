724e4d7b1ccffdaac0faf81c52ec68a707af4f4b
YatharthDedhia <dedhiayatharth2004@gmail.com>
2023-01-25 16:44:18 +0530

    Added Performance graph

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,10 +1,20 @@
 
 // import React from "react";
 import React, { useState } from 'react';
+import {
+    LineChart,
+    ResponsiveContainer,
+    Legend, Tooltip,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid
+} from 'recharts';
 import Popup from 'reactjs-popup';
 import './css/profile.css'
 import './css/line-chart.css'
 import './css/ranklist.css'
+import './css/graph.css'
 import axios from 'axios';
 import line from "simple-line-chart";
 import { Chart, ChartSeries, ChartSeriesItem, ChartCategoryAxis, ChartCategoryAxisItem } from '@progress/kendo-react-charts';
@@ -72,58 +82,167 @@ const Profile_Block = () => {
                 <p class="scnd-font-color">Email ID : {emailID}</p>
                 <p class="scnd-font-color">UserName : {userName}</p>
             </div>
-            <input type="file" onChange={(e) => setImage(e.target.files[0])}></input>
+
+            <div class="upload-btn-wrapper">
+                <button class="btn">Upload a file</button>
+                <input type="file" onChange={(e) => setImage(e.target.files[0])} name="myfile" />
+            </div>
+            {/* <button className="btn">Upload a file</button>
+            <input type="file" ></input> */}
             <button onClick={submitImage} className='file-button'>Upload image as profile Photo</button>
         </div>
     );
 };
 
-const Line_Graph = () => {
-    return (
-        <div class="line-chart-block block clear">
-            {/* <!-- LINE CHART BLOCK (LEFT-CONTAINER) --> */}
-            <div class="line-chart">
-                {/* <!-- LINE-CHART by @kseso https://codepen.io/Kseso/pen/phiyL --> */}
-                <div class='grafico'>
-                    <ul class='eje-y'>
-                        <li data-ejeY='30'></li>
-                        <li data-ejeY='20'></li>
-                        <li data-ejeY='10'></li>
-                        <li data-ejeY='0'></li>
-                    </ul>
-                    <ul class='eje-x'>
-                        <li>Apr</li>
-                        <li>May</li>
-                        <li>Jun</li>
-                    </ul>
-                    <span data-valor='25'>
-                        <span data-valor='8'>
-                            <span data-valor='13'>
-                                <span data-valor='5'>
-                                    <span data-valor='23'>
-                                        <span data-valor='12'>
-                                            <span data-valor='15'>
-                                            </span></span></span></span></span></span></span>
-                </div>
-                <ul class="month-data clear">
-                    <li>
-                        <p>APR<span class="scnd-font-color"> 2013</span></p>
-                        <p><span class="entypo-plus increment"> </span>21<sup>%</sup></p>
-                    </li>
-                    <li>
-                        <p>MAY<span class="scnd-font-color"> 2013</span></p>
-                        <p><span class="entypo-plus increment"> </span>48<sup>%</sup></p>
-                    </li>
-                    <li>
-                        <p>JUN<span class="scnd-font-color"> 2013</span></p>
-                        <p><span class="entypo-plus increment"> </span>35<sup>%</sup></p>
-                    </li>
-                </ul>
-            </div>
-        </div>
+const Graph = () => {
+    // const pdata = [
+    //     {
+    //         name: 'MongoDb',
+    //         student: 11,
+    //         fees: 120
+    //     },
+    //     {
+    //         name: 'Javascript',
+    //         student: 15,
+    //         fees: 12
+    //     },
+    //     {
+    //         name: 'PHP',
+    //         student: 5,
+    //         fees: 10
+    //     },
+    //     {
+    //         name: 'Java',
+    //         student: 10,
+    //         fees: 5
+    //     },
+    //     {
+    //         name: 'C#',
+    //         student: 9,
+    //         fees: 4
+    //     },
+    //     {
+    //         name: 'C++',
+    //         student: 10,
+    //         fees: 8
+    //     },
+    // ];
 
+    const pdata = [
+        {
+            "PaperCode": 101,
+            "TestName": "React",
+            "Date": "2023-01-20T00:00:00.000Z",
+            "StartTime": "1970-01-01",
+            "EndTime": "1970-01-01T08:00:00.000Z",
+            "marks": 3
+        },
+        {
+            "PaperCode": 102,
+            "TestName": "Node",
+            "Date": "2023-02-14T00:00:00.000Z",
+            "StartTime": "1970-01-01",
+            "EndTime": "1970-01-01T06:30:00.000Z",
+            "marks": 15
+        },
+        {
+            "PaperCode": 103,
+            "TestName": "Express",
+            "Date": "2023-10-14T00:00:00.000Z",
+            "StartTime": "1970-01-01",
+            "EndTime": "1970-01-01T13:30:00.000Z",
+            "marks": 10
+        },
+        {
+            "PaperCode": 103,
+            "TestName": "Express",
+            "Date": "2023-10-14T00:00:00.000Z",
+            "StartTime": "1970-01-01",
+            "EndTime": "1970-01-01T13:30:00.000Z",
+            "marks": 9
+        },
+        {
+            "PaperCode": 103,
+            "TestName": "Express",
+            "Date": "2023-10-14T00:00:00.000Z",
+            "StartTime": "1970-01-01",
+            "EndTime": "1970-01-01T13:30:00.000Z",
+            "marks": 20
+        },
+        {
+            "PaperCode": 103,
+            "TestName": "Express",
+            "Date": "2023-10-14T00:00:00.000Z",
+            "StartTime": "1970-01-01",
+            "EndTime": "1970-01-01T13:30:00.000Z",
+            "marks": 25
+        },
+        {
+            "PaperCode": 103,
+            "TestName": "Express",
+            "Date": "2023-10-14T00:00:00.000Z",
+            "StartTime": "1970-01-01",
+            "EndTime": "1970-01-01T13:30:00.000Z",
+            "marks": 13
+        }
+    ]
+
+    pdata.map((f) => {
+        f["StartTime"] = (parseInt(f.StartTime.slice(8, 10))).toString() + "/" + (parseInt(f.StartTime.slice(5, 7))).toString();
+    })
+
+    const graph_data = []
+    useEffect(async () => {
+        axios.get("https://lmsapiv01.azurewebsites.net/api/attemptedlist/4")
+            .then((response) => {
+
+                response.data[0].map((f) => {
+                    var num = f.PaperCode
+                    var str = num.toString()
+
+                    axios
+                        .get("https://lmsapiv01.azurewebsites.net/api/totalmarksallstuds/" + str)
+                        .then((response2) => {
+                            // console.log(response2.data[0])
+                            response2.data[0].map((res) => {
+                                if (res.UserID === 4) {
+                                    f["marks"] = res.MarksScored;
+                                    f["StartTime"] = f.StartTime.slice(0, 10);
+                                    f["StartTime"] = f.StartTime.slice(8, 10) + " " + f.StartTime.slice(5, 7) + " " + f.StartTime.slice(0, 4);
+                                }
+                            })
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        });
+                    // console.log(f)
+                    graph_data.push(f)
+                    console.log(graph_data)
+                })
+            })
+    }, [])
+
+    return (
+        <div className='graph-head'>
+            <h1 className="text-heading">
+                Performance Graph
+            </h1>
+
+            {console.log("he")}
+            {console.log(graph_data)}
+
+            <ResponsiveContainer width="100%" aspect={3}>
+                <LineChart data={pdata} margin={{ right: 300 }}>
+                    <Line type="monotone" dataKey="marks" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                    <XAxis dataKey="TestName" stroke="white" />
+                    <YAxis stroke="white" />
+                    <Tooltip />
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
     );
-};
+}
 
 const RankList = () => {
 
@@ -154,34 +273,34 @@ const RankList = () => {
         }
     ];
 
-    const [obj, setObj] = useState([])
+    const [obj, setObj] = useState(testObj)
     let temparr = []
 
-    useEffect(async () => {
-        axios.get("https://lmsapiv01.azurewebsites.net/api/attemptedlist/4")
-            .then((response) => {
+    // useEffect(async () => {
+    //     axios.get("https://lmsapiv01.azurewebsites.net/api/attemptedlist/4")
+    //         .then((response) => {
 
-                response.data[0].map((f) => {
-                    var num = f.PaperCode
-                    var str = num.toString()
+    //             response.data[0].map((f) => {
+    //                 var num = f.PaperCode
+    //                 var str = num.toString()
 
-                    axios
-                        .get("https://lmsapiv01.azurewebsites.net/api/totalmarksallstuds/" + str)
-                        .then((response2) => {
-                            console.log(response2.data[0])
-                            response2.data[0].map((res) => {
-                                temparr.push(res)
-                            })
-                        })
-                        .catch((err) => {
-                            console.log(err);
-                        });
-                })
-                console.log(temparr);
-                console.log(obj);
-            })
-        setObj(temparr);
-    }, [])
+    //                 axios
+    //                     .get("https://lmsapiv01.azurewebsites.net/api/totalmarksallstuds/" + str)
+    //                     .then((response2) => {
+    //                         console.log(response2.data[0])
+    //                         response2.data[0].map((res) => {
+    //                             temparr.push(res)
+    //                         })
+    //                     })
+    //                     .catch((err) => {
+    //                         console.log(err);
+    //                     });
+    //             })
+    //             console.log(temparr);
+    //             console.log(obj);
+    //         })
+    //     setObj(temparr);
+    // }, [])
 
     return (
         <div class="ranklist-container">
@@ -225,7 +344,7 @@ const Profile = () => {
         <div className='main-container'>
             <div className='container3'>
                 <Profile_Block />
-                <Line_Graph />
+                <Graph />
                 <RankList />
             </div>
         </div>

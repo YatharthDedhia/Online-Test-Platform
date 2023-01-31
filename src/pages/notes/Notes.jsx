e7fa4046bd8971f56aff675334f27a865f5c1717
YatharthDedhia <dedhiayatharth2004@gmail.com>
2023-01-31 20:17:47 +0530

    added notes display

--- src/pages/notes/Notes.jsx
+++ src/pages/notes/Notes.jsx
@@ -6,40 +6,37 @@ import logo from '../../Images/logo-no-background.png'
 import './css/notes.css'
 const testObj = [
   {
-"CourseId": 2,
-"CourseName": "Maths",
-"CourseCode": 600,
-"Notes": "www.drive2.com",
-"Image": "https://cache.careers360.mobi/media/article_images/2022/1/14/_NCERT-Book-for-Class-12-Maths.jpg",
-"Syllabus": "https://www.amazon.in/Advanced-Engineering-Mathematics-H-C-Taneja/dp/9389307325/ref=sr_1_2_sspa?crid=1T04C756ZZXZ0&keywords=Engineering+maths&qid=1675101764&sprefix=engineering+maths%2Caps%2C304&sr=8-2-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1 "
+    "CourseId": 2,
+    "CourseName": "Maths",
+    "CourseCode": 600,
+    "Notes": "www.drive2.com",
+    "Image": "https://cache.careers360.mobi/media/article_images/2022/1/14/_NCERT-Book-for-Class-12-Maths.jpg",
+    "Syllabus": "https://www.amazon.in/Advanced-Engineering-Mathematics-H-C-Taneja/dp/9389307325/ref=sr_1_2_sspa?crid=1T04C756ZZXZ0&keywords=Engineering+maths&qid=1675101764&sprefix=engineering+maths%2Caps%2C304&sr=8-2-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1 "
   }];
 const Notes = () => {
   const [label, setlabel] = useState(3);
-  const [link1, setLink1] = useState("");
-  const [link2, setLink2] = useState("");
-  const [link3, setLink3] = useState("");
-  const [link4, setLink4] = useState("");
-  const [courseName, setcourseName] = useState("");
-  const [linkimage, setLinkImage] = useState("");
-  const [syllabusimg, setSyllabusimg] = useState("");
   const [obj, setObj] = useState(testObj);
   const [courseList, setCourselist] = useState([]);
-  const [newType, setNewType] = useState(1)
-  let temparr = []
 
   useEffect(async () => {
 
     axios.get("https://lmsapiv01.azurewebsites.net/api/course")
       .then((response) => {
-        // console.log(response.data[0]);
         setObj(response.data[0])
       })
 
-    axios.get("https://lmsapiv01.azurewebsites.net/api/course")
+    // axios.get("https://lmsapiv01.azurewebsites.net/api/course")
+    //   .then((response) => {
+    //     setCourselist(response.data[0]);
+    //   })
+
+    axios.get("https://lmsapiv01.azurewebsites.net/api/student/courses/" + String(JSON.parse(localStorage.getItem('login')).user.UserId))
       .then((response) => {
         setCourselist(response.data[0]);
       })
   }, [])
+
+
   const Header_Menu = () => {
     return (
       <header class="block-institute">
@@ -68,28 +65,43 @@ const Notes = () => {
   };
   return (
     <div>
-      <Header_Menu/>
+      <Header_Menu />
       <header>
         <br />
         <h1>Courses List</h1>
         <br />
       </header>
       <div className="flexboxcontainer">
-          {obj.map((e) => {
-              return (
-                // <div className="container10">
-                <div className="container9">
-                  <a className="subimg1" href={e.Syllabus}>
-                  <img src={e.Image}/>
-                  </a>
-                  {/* <a href={e.Link}> */}
-                  <div className="buttonlink"><a href={e.Notes}><button className="buttonedu"><span className="icon fontawesome-pencil scnd-font-color"></span>{e.CourseName}</button></a></div>
-                  {/* </a> */}
-                  </div>
-                // </div>
-              )
+        {obj.map((e) => {
+          return (
+            <div>
 
-          })}
+              {courseList.map((course) => {
+                if (course.CourseId === e.CourseId) {
+                  return (
+                    <div className="container9">
+                      <a className="subimg1" href={e.Syllabus}>
+                        <img src={e.Image} />
+                      </a>
+                      <div className="buttonlink">
+                        <a href={e.Notes}>
+                          <button className="buttonedu">
+                            <span className="icon fontawesome-pencil scnd-font-color" />
+                            {e.CourseName}
+                          </button>
+                        </a>
+                      </div>
+                    </div>
+                  )
+                }
+                else {
+                  return (null)
+                }
+              })}
+            </div>
+          )
+
+        })}
       </div>
     </div>
   )

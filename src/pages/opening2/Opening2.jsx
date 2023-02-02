2e063b8fd208cdedb96d30c943494f6031521561
YatharthDedhia <dedhiayatharth2004@gmail.com>
2023-02-02 21:14:38 +0530

    fixed opening2 page

--- src/pages/opening2/Opening2.jsx
+++ src/pages/opening2/Opening2.jsx
@@ -7,46 +7,37 @@ import { Helmet } from 'react-helmet';
 // const { player } = Player.getState();
 // console.log(player.currentTime); // print current time
 class Opening2 extends React.Component {
-    componentDidMount = () => {
-      this.playVideo();
-    };
+  componentDidMount = () => {
+    this.playVideo();
+  };
 
-    componentWillUnmount = () => {
-        this.pauseVideo();
-    };
+  componentWillUnmount = () => {
+    this.pauseVideo();
+  };
 
 
-    playVideo = () => {
-      // You can use the play method as normal on your video ref
-      this.refs.vidRef.play();
-    };
+  playVideo = () => {
+    // You can use the play method as normal on your video ref
+    this.refs.vidRef.play();
+  };
 
-    pauseVideo = () => {
-      // Pause as well
-      this.refs.vidRef.pause();
-    };
-    render = () => {
-        return (
-          <div className='backg'>
-            <meta http-equiv = "refresh" content = "5; url = /landing" />
-            <div className="videoc">
-            <video width="800" height="600"  autoplay loop
-              ref="vidRef"
-              src={video}
-              type="video/mp4"
-            />
-            </div>
-
-            {/* <div>
-              <button onClick={this.playVideo}>
-                Play!
-              </button>
-              <button onClick={this.pauseVideo}>
-                Pause!
-              </button>
-            </div> */}
-          </div>
-        );
-      };
-    }
+  pauseVideo = () => {
+    // Pause as well
+    this.refs.vidRef.pause();
+  };
+  render = () => {
+    return (
+      <div className='backg'>
+        <meta http-equiv="refresh" content="10; url = /landing" />
+        <div className="videoc">
+          <video width="800" height="600" autoplay loop
+            ref="vidRef"
+            src={video}
+            type="video/mp4"
+          />
+        </div>
+      </div>
+    );
+  };
+}
 export default Opening2;
